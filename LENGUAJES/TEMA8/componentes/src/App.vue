78427dc45fc478448cff1f4bd94020cfdfa4c1e3
Dani <dremonj@iesch.org>
2026-05-14 15:54:39 +0200

    AutoPush

---
+++ LENGUAJES/TEMA8/componentes/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import BlogPost from './componentes/BlogPost.vue';
+import BotonContador from './componentes/BotonContador.vue';
+import { ref } from 'vue';
+
+const posts = ref([
+  {id:1, title: "Post 01", contenido:"Contenido del post 01"},
+  {id:2, title: "Post 02", contenido:"Contenido del post 02"},
+  {id:3, title: "Post 03", contenido:"Contenido del post 03"},
+  {id:4, title: "Post 04"},
+  {id:5, title: "Post 03", contenido:"Contenido del post 03"},
+
+])
+
+const miFavorito = ref("")
+const cambiarFavorito = (titulo) => {
+  miFavorito.value = titulo
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>DAM 1 - 2026</h1>
+    
+    <h2>Mi post favorito: {{ miFavorito }}</h2>
+    <hr>
+    <BlogPost 
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :contenido="post.contenido"
+      :id="post.id"
+      @cambiarTituloFavorito="cambiarFavorito"
+      class="mb-2"
+      />
+    
+  </div>
+  
+</template>
+
+<style scoped></style>
---
+++ LENGUAJES/TEMA8/componentes/src/componentes/BlogPost.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+    // defineProps(['title', 'contenido', 'id', 'colorText'])
+    const props = defineProps({
+        title: String,
+        id: String,
+        contenido: {
+            type: String,
+            default: 'Sin descripcion'
+
+        }
+    })
+    
+    
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">
+                {{title}}
+            </h4>
+            <p>
+                {{ id }} - {{ contenido }}
+            </p>
+            <button 
+                @click="$emit('cambiarTituloFavorito', title)"
+                class="btn btn-outline-primary">
+                    Mi favorito
+            </button>
+        </div>
+    </div>
+</template>
