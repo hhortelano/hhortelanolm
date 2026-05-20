848a1c24589bc040e2ae0562a987071067f5dbe3
Dani <dremonj@iesch.org>
2026-05-20 17:38:13 +0200

    AutoPush

--- LENGUAJES/TEMA8/componentes/src/App.vue
+++ LENGUAJES/TEMA8/componentes/src/App.vue
@@ -1,41 +1,105 @@
 <script setup>
+
 import BlogPost from './componentes/BlogPost.vue';
 import BotonContador from './componentes/BotonContador.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import PaginatePost from './componentes/PaginatePost.vue';
+import LoadingSpinner from './componentes/LoadingSpinner.vue';
 
-const posts = ref([
-  {id:1, title: "Post 01", contenido:"Contenido del post 01"},
-  {id:2, title: "Post 02", contenido:"Contenido del post 02"},
-  {id:3, title: "Post 03", contenido:"Contenido del post 03"},
-  {id:4, title: "Post 04"},
-  {id:5, title: "Post 03", contenido:"Contenido del post 03"},
-
-])
+const posts = ref([])
 
 const miFavorito = ref("")
+
 const cambiarFavorito = (titulo) => {
   miFavorito.value = titulo
 }
+
+// Paginacion
+const postsXPagina = 10;
+
+const inicio = ref(0);
+
+const fin = ref(postsXPagina)
+
+const tamanoPost = computed(() => posts.value.length)
+
+const loading = ref(true)
+
+// Metodos para la paginacion
+
+const next = () => {
+  if (fin.value > posts.value.length) {
+
+  } else {
+    inicio.value = inicio.value + postsXPagina
+    fin.value = fin.value + postsXPagina
+  }
+
+}
+
+const previous = () => {
+  if (inicio.value > 0) {
+    inicio.value = inicio.value - postsXPagina
+    fin.value = fin.value - postsXPagina
+  } else {
+
+  }
+
+
+}
+
+// Vamos a usar Fetch para cargar una APi Publica
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+
+  // Lo que nosotros formateamos en json
+  .then(response => response.json())
+
+  //.then( data => console.log(data))
+  .then(data => posts.value = data)
+
+  .finally(
+    setTimeout( () => {
+      loading.value = false
+    }, 1000)
+    )
+
+
 </script>
 
 <template>
   <div class="container">
+
     <h1>DAM 1 - 2026</h1>
-    
+
     <h2>Mi post favorito: {{ miFavorito }}</h2>
-    <hr>
-    <BlogPost 
-      v-for="post in posts"
-      :key="post.id"
-      :title="post.title"
+
+    <LoadingSpinner v-if="loading" />
+    <div class="container">
+      <PaginatePost 
+      class="mb-2" 
+      @next="next" 
+      @previous="previous" 
+      :inicio="inicio" 
+      :fin="fin" 
+      :tamanio="tamanoPost" 
+      />
+      
+      <BlogPost 
+      v-for="post in 
+      posts.slice(inicio, fin)" 
+      :key="post.id" 
+      :title="post.title" 
       :contenido="post.contenido"
-      :id="post.id"
-      @cambiarTituloFavorito="cambiarFavorito"
+      :id="post.id" 
+      @cambiarTituloFavorito="cambiarFavorito" 
       class="mb-2"
       />
-    
+    </div>
+
+
   </div>
-  
+
 </template>
 
 <style scoped></style>
---
+++ LENGUAJES/TEMA8/componentes/src/componentes/PaginatePost.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const emit = defineEmits(["next", "previous"])
+    defineProps(["inicio", "fin", "tamanio"])
+</script>
+
+<template>
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button 
+        type="button" 
+        class="btn btn-outline-primary" 
+        @click="emit('previous')"
+        :disabled="inicio === 0"
+
+        >
+        Anterior {{ inicio }}
+        </button>
+        
+        <button 
+        type="button" 
+        class="btn btn-outline-primary" 
+        @click="emit('next')"
+        :disabled="fin >= tamanio"
+        >
+        Siguiente {{ fin }}
+        </button>
+    </div>
+</template>
